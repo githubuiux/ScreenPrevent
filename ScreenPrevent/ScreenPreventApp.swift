1ad699615b4cd9b75ebc6ed35f07b4129acad62c
githubuiux <uiuxista@gmail.com>
2023-07-15 18:39:24 +0300

    Initial Commit

---
+++ ScreenPrevent/ScreenPreventApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenPreventApp.swift
+//  ScreenPrevent
+//
+//  Created by Levent on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScreenPreventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
